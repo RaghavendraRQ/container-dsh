f5cf0667805dc09adca075405cbbf26333c9e4a0
RaghavnedraRQ <sai783191@gmail.com>
2025-05-01 17:15:42 +0530

    feat(logger): Add timeseries logger for metrics

---
+++ logger/time.go
@@ -0,0 +1,53 @@
+package logger
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"sync"
+	"time"
+)
+
+type MetricEntry struct {
+	TimeStamp   time.Time `json:"timestamp"`
+	ContainerId string    `json:"container_id"`
+	Metric      string    `json:"metric"`
+	Value       float64   `json:"value"`
+}
+
+type TimeSeries struct {
+	mu       sync.Mutex
+	buffer   []MetricEntry
+	filePath string
+}
+
+func (t *TimeSeries) Log(id, metric string, value float64) {
+	t.mu.Lock()
+	defer t.mu.Unlock()
+	newMetric := MetricEntry{
+		TimeStamp:   time.Now(),
+		ContainerId: id,
+		Metric:      metric,
+		Value:       value,
+	}
+	t.buffer = append(t.buffer, newMetric)
+}
+
+func (t *TimeSeries) Flush(filepath string) {
+	t.filePath = filepath
+	file, err := os.OpenFile(t.filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		log.Println("Open file error:", err)
+		return
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	for _, metric := range t.buffer {
+		if err := encoder.Encode(metric); err != nil {
+			fmt.Printf("Error while inserting log: %s", err)
+		}
+	}
+	t.buffer = nil
+}
--- main.go
+++ main.go
@@ -7,56 +7,62 @@ import (
 	"log"
 	"time"
 
+	"container-dsh/logger"
+
 	cont "github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
 )
 
 func main() {
-    ctx := context.Background()
-    cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
-    if err != nil {
-        log.Fatalln("Error: ", err)
-    }
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
+	if err != nil {
+		log.Fatalln("Error: ", err)
+	}
 
-    defer cli.Close()
+	defer cli.Close()
 
-    containers, err := cli.ContainerList(ctx, cont.ListOptions{All:true})
-    if err != nil {
-        log.Fatalln("Error: ", err)
-    }
+	containers, err := cli.ContainerList(ctx, cont.ListOptions{All: true})
+	if err != nil {
+		log.Fatalln("Error: ", err)
+	}
+	timeSeriesLogger := logger.TimeSeries{}
+	for _, container := range containers {
 
-    for _, container := range containers {
+		go func(id string) {
+			stats, err := cli.ContainerStatsOneShot(ctx, id)
+			if err != nil {
+				log.Fatalln("Error in ContainerStatsOneShot: ", err)
+			}
+			defer stats.Body.Close()
 
-        go func(id string) {
-            stats, err := cli.ContainerStatsOneShot(ctx, id)
-            if err != nil {
-                log.Fatalln("Error in ContainerStatsOneShot: ", err)
-            }
-            defer stats.Body.Close()
-            
-            var statsData cont.StatsResponse
-            if err := json.NewDecoder(stats.Body).Decode(&statsData); err != nil {
-                fmt.Println("Decode err: ", err)
-                return 
-            }
-            fmt.Printf("[Container %s] CPU: %.2f%% | Mem: %.2fMB\n",
-                id[:10],
-                calculateCPUPercent(&statsData),
-                float64(statsData.MemoryStats.Usage)/(1024*1024),
-            )
+			var statsData cont.StatsResponse
 
-        } (container.ID)
+			if err := json.NewDecoder(stats.Body).Decode(&statsData); err != nil {
+				fmt.Println("Decode err: ", err)
+				return
+			}
+			fmt.Printf("[Container %s] CPU: %.2f%% | Mem: %.2fMB\n",
+				id[:10],
+				calculateCPUPercent(&statsData),
+				float64(statsData.MemoryStats.Stats["Usage"])/(1024*1024),
+			)
+			timeSeriesLogger.Log(id[:10], "CPU", calculateCPUPercent(&statsData))
+			timeSeriesLogger.Log(id[:10], "Mem", float64(statsData.MemoryStats.Stats["Usage"])/(1024*1024))
 
-    }
+		}(container.ID)
 
-    time.Sleep(3 * time.Second)
+	}
+
+	time.Sleep(3 * time.Second)
+	timeSeriesLogger.Flush("test.json")
 }
 
 func calculateCPUPercent(stat *cont.StatsResponse) float64 {
-    cpuDelta := float64(stat.CPUStats.CPUUsage.TotalUsage - stat.PreCPUStats.CPUUsage.TotalUsage)
-    systemDelta := float64(stat.CPUStats.SystemUsage - stat.PreCPUStats.SystemUsage)
-    if systemDelta > 0.0 && cpuDelta > 0.0 {
-        return (cpuDelta / systemDelta) * float64(len(stat.CPUStats.CPUUsage.PercpuUsage)) * 100.0
-    }
-    return 0.0
+	cpuDelta := float64(stat.CPUStats.CPUUsage.TotalUsage - stat.PreCPUStats.CPUUsage.TotalUsage)
+	systemDelta := float64(stat.CPUStats.SystemUsage - stat.PreCPUStats.SystemUsage)
+	if systemDelta > 0.0 && cpuDelta > 0.0 {
+		return (cpuDelta / systemDelta) * float64(len(stat.CPUStats.CPUUsage.PercpuUsage)) * 100.0
+	}
+	return 0.0
 }
