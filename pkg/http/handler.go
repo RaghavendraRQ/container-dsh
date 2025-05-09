c741ca1eb8385811a243d64f09cc12432164d4ab
RaghavnedraRQ <sai783191@gmail.com>
2025-05-09 13:51:12 +0530

    feat(http): Metrics endpoint GETS containerid's for now

--- pkg/http/handler.go
+++ pkg/http/handler.go
@@ -1,6 +1,7 @@
 package http
 
 import (
+	"container-dsh/internal/container"
 	"log"
 	"net/http"
 
@@ -13,6 +14,33 @@ const (
 
 func Run() {
 	muxRouter := mux.NewRouter()
+	muxRouter.Use(loggerMiddleWare)
+	muxRouter.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to the Container Metrics API"))
+	}).Methods("GET")
+	muxRouter.HandleFunc("/metrics", GetMetric).Methods("GET")
 	log.Println("Starting HTTP server on port", URL)
 	http.ListenAndServe(URL, muxRouter)
+}
+
+func loggerMiddleWare(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("Received request:", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func GetMetric(w http.ResponseWriter, r *http.Request) {
+	cli := container.GetClient()
+	containers, err := container.GetContainerList(cli)
+	if err != nil {
+		panic(err)
+	}
+
+	w.Write([]byte("Container IDs: "))
+	for _, containerId := range containers {
+		w.Write([]byte(containerId + "\n"))
+	}
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Metrics"))
 }
