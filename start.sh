d3e3cf73ffdc42052a39fe84fecb13ff4e9de28f
RaghavnedraRQ <sai783191@gmail.com>
2025-05-21 23:36:49 +0530

    style(start script): Now change according to main

--- start.sh
+++ start.sh
@@ -1,11 +1,13 @@
 > time.json
 > time2.json
 
-if [ "$1" == "test" ]; then
-    go run ./cmd/mock
-elif  [ "$1" == "server" ]; then
-    go run ./cmd/server
-else
-    go run ./cmd/container-dsh
-fi
+# if [ "$1" == "test" ]; then
+    # go run ./cmd/mock
+# elif  [ "$1" == "server" ]; then
+    # go run ./cmd/server
+# else
+    # go run ./cmd/container-dsh
+# fi
+
+go run ./cmd/container-dsh --mode="$1"
 jq . time.json > time2.json
