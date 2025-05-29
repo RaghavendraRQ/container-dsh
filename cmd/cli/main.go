feb761ad889ca91cbf009e7a4a4764ededd2b970
RaghavnedraRQ <sai783191@gmail.com>
2025-05-29 23:33:21 +0530

    feat(cli): Add bubbletea to the project

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type Model struct {
+	choices  []string
+	cursor   int
+	selected map[int]struct{}
+}
+
+func (m *Model) Init() tea.Cmd {
+	return nil
+}
 
 func main() {
 	fmt.Println("CLI VERSION")
+
+}
+
+func initialModel() Model {
+	return Model{
+		choices:  []string{"Show containers", "show images", "show stats"},
+		selected: make(map[int]struct{}),
+	}
 }
